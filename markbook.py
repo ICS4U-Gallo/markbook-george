a2bf3a9f8c6591db57d07c016cb9cdc76cde4002
F190 <figo781802514@gmail.com>
2019-09-18 13:39:33 -0400

    pull from teacher

--- markbook.py
+++ markbook.py
@@ -5,7 +5,7 @@ Group members: Figo, Johnson, Geroge
 from typing import Dict
 
 
-def create_assignment(name, due, points):
+def create_assignment(name: str, due: str, points: int):
     """Creates an assignment represented as a dictionary
     
     Args:
@@ -32,3 +32,32 @@ def calculate_average_mark(student):
         total += int(mark)
     ave = total/len(student["marks"])
     return ave
+
+def add_student_to_classroom(student, classroom):
+    """Adds student to a classroom
+    Args:
+        student: Student dict
+        classroom: The classroom to add the student to
+    """
+    pass
+
+
+def remove_student_from_classroom(student: Dict, classroom: Dict):
+    """Removes student from classroom
+    Args:
+        student: The student to be removed
+        classroom: the class from which the student will be removed.
+    """
+    pass
+
+
+def edit_student(student: Dict, **kwargs: Dict):
+    """Edits the student's info with the provided key/value pairs
+    Args:
+        student: The student whose data needs to be udated.
+        **kwargs: KeyWordARGumentS. The key/value pairs of the
+            data that needs to be changed. Can come in the form
+            of a dictionary.
+    """
+    pass
+
