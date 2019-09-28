e99747d7e7c7e8df81da3fa2d441293a67128e2a
F190 <43610088+F190@users.noreply.github.com>
2019-09-28 18:11:39 -0400

    Update markbook.py
    main method change

--- markbook.py
+++ markbook.py
@@ -5,7 +5,7 @@ Group members: Figo, Johnson, Geroge
 from typing import Dict
 
 
-def create_assignment(name: str, due: str, points: int) -> Dict:
+def create_assignment(name: str, due: str, points: int,course_code:int) -> Dict:
     """Creates an assignment represented as a dictionary
     
     Args:
@@ -15,15 +15,15 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
     Returns:
         Assignment as a dictionary.
     """
-    return {"name": name, "due": due, "points": points}
+    return {"name": name, "due": due, "points": points , "course_code":int}
 
 
 def create_classroom(course_code, course_name, period, teacher):
-    """Creates a classroom dictionary"""
-    return {"CourseCode": couse_code, "CourseName": couse_name, "Period": 3, 
-    "Teacher": teacher, "student_list": []}
 
-    return {"CourseCode": couse_code, "CourseName": couse_name, "Period": period, "Teacher": teacher}
+    """Creates a classroom dictionary"""
+    
+    return {"CourseCode": couse_code, "CourseName": couse_name, "Period": 3, 
+    "Teacher": teacher, "student_list": [],"assignment_list":[]}
 
 
 def calculate_average_mark(student):
@@ -67,4 +67,142 @@ def edit_student(student: Dict, **kwargs: Dict):
     """
 
     pass
+
+while True: 
+    menu = "menu"
+    if menu == "menu":
+        print("Type 'assignment' to create an assignment")
+        print("Type 'classroom' to create a classroom")
+        print("Type 'mark' to calculate average mark")
+        print("Type 'Astudent' to add student to classroom")
+        print("Type 'Rstudent' to remove student from cmenulassroom")
+        print("Type 'Estudent' to edit student in classroom")
+        menu = input()
+
+    if menu == "assignment":
+        create_assignment()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+
+    if menu == "classroom":
+        create_classroom()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+
+    if menu == "mark":
+        calculate_average_mark()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+
+    if menu == "Astudent":
+        add_student_to_classroom()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+
+    if menu == "Rstudent":
+        remove_student_from_classroom()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+
+    if menu == "Estudent":
+        edit_student()
+        print("Type 'menu' to get back to menu")
+        menu = input()
+    if menu =="UGstudent":
+        update_student_mark()
+
+
+
+
+class Student:
+    def __init__(self,st_name str,st_lastname str, gender, image, st_number, email str, assignment dict):
+        self.st_name = st_name
+        self.st_lstname = st_lastname
+        self.gender = gender
+        self.image = image
+        self.st_number = st.st_number
+        self.email = email
+        self.assignment = assignment
+
+    def get_name(self ,name):
+        return(st_name)
+
+    def get_lastname(self, lastname):
+        return(st_lastname)
+
+    def get_gender(self, gender):
+        return(gender)
+
+    def get_image(self, image):
+        return(image)
+
+    def get_number(self, number):
+        return(st_number)
+
+    def get_email(self, email):
+        return(email)
+
+    def update_st(self,st_name ,st_lastname, gender, image, st_number, email, assignment):
+        self.st_name = st_name
+        self.st_lstname = st_lastname
+        self.gender = gender
+        self.image = image
+        self.st_number = st.st_number
+        self.email = email
+        self.assignment = assignment
+}
+
+
+
+    def save_grade(self, assignment_name , st_grade)
+
+
+class Classroom:
+    def __init__(self, course_code, teacher_name, period, student_list):
+        self.course_code = course_code
+        self.teacher_name = teacher_name
+        self.period = period
+        self.student_list = student_list
+
+
+def student_info_create(){
+    st_listofcourse=
+    st_name=input("enter the student name")
+    ...
+    studentx={"studentname":st_name , "student_lasyname":st_lname }
+    for coursse in st_list..:
+        for coures in classrooms
+            add_student_to_classroom(studentx, classroom):
+    
+
+}
+#question:how to specify which assignment is for what classroom
+
+def update_student_mark():
+inp_number=input("enter the student name")
+course_code=input()
+assignment_name=
+grade=input()
+for classroom in class_list:{
+    if class_list[classroom]["student_list"][st_number]==inp_number:
+        class_list[classroom]["student_list"]["assigment_list"]["assignment_name"]=grade
+
+}
+
+
+"student_list": []
+#save dictionaries and information into the json file
+
+def first_write_to_file(str filepath , studententery ,..):{
+    if studententery notnull
+    write_to student
+
+    write_to_classroom()
+
+
+}
+def write_to_class():
+    create_classroom()
+
+
 
